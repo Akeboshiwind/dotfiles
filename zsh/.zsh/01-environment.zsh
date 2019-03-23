a88738eae04c1a9e1566e0ec5369ba055218d019
Oliver Marshall <olivershawmarshall@gmail.com>
2019-03-23 20:41:02 +0000

    Add GPG config to zsh config

--- zsh/.zsh/01-environment.zsh
+++ zsh/.zsh/01-environment.zsh
@@ -5,7 +5,17 @@
 
 export PATH=$PATH:/usr/local/bin:/usr/local/share/dotnet:~/dotnet/tools:~/bin
 
+
+
 # >> Editor
 
 export EDITOR=vim
 export VISUAL=vim
+
+
+
+#>> GPG
+
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
