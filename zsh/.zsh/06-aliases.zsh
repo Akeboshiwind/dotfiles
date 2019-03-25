b885a3b8f5482785c4d6d5d32d77ecaf67bdf5bb
Oliver Marshall <olivershawmarshall@gmail.com>
2019-03-25 10:50:24 +0000

    Add some aliases

--- zsh/.zsh/06-aliases.zsh
+++ zsh/.zsh/06-aliases.zsh
@@ -1,4 +1,29 @@
 # 06-aliases.zsh
 
 
-# >> Stuff
+# >> General
+
+alias mkdir="mkdir -p"
+alias fuck='sudo $(fc -ln -1)'
+
+
+
+# >> Git
+
+alias g="git"
+alias ga="git add"
+alias gc="git commit -m"
+alias gs="git status"
+alias gd="git diff"
+alias gps="git push"
+alias gpl="git pull"
+
+
+
+# >> Kubernetes
+
+alias k="kubectl"
+
+alias h="helm"
+alias ht="helm --tiller-namespace turing"
+
