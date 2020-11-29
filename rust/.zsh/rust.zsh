2f3f0953faa0cca78026a13ed91bccb65bc83221
Oliver Marshall <olivershawmarshall@gmail.com>
2020-11-29 15:47:18 +0000

    Add rust and completions

---
+++ rust/.zsh/rust.zsh
@@ -0,0 +1,39 @@
+# >> Rust completions
+
+
+# >> Config
+
+cargo_completion_path=$COMPLETION_PATH/_cargo
+rustup_completion_path=$COMPLETION_PATH/_rustup
+
+
+
+# Only install the completions if rustup is installed
+command -v rustup 1>/dev/null && {
+    installed_completions=false
+
+    # >> Rustup
+
+    [ -e "$rustup_completion_path" ] || {
+        info "Installing Rustup completions..."
+        rustup completions zsh rustup > $rustup_completion_path
+        info "Installed!"
+        installed_completions=true
+    }
+
+
+
+    # >> Cargo
+
+    [ -e "$cargo_completion_path" ] || {
+        info "Installing Cargo completions..."
+        rustup completions zsh cargo > $cargo_completion_path
+        info "Installed!"
+        installed_completions=true
+    }
+
+    [ $installed_completions = 'true' ] && {
+        war "Installed rust completions, restart shell to see them working"
+    }
+}
+
