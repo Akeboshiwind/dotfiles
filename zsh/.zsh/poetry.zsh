cab6f78d54df69280a417bd4dd880a55b4ff69c6
Oliver Marshall <olivershawmarshall@gmail.com>
2022-01-28 10:29:22 +0000

    Support poetry in zsh config

---
+++ zsh/.zsh/poetry.zsh
@@ -0,0 +1,20 @@
+# poetry.zsh
+
+
+# >> Setup path
+
+export PATH=$PATH:/Users/oliverm/Library/Python/3.9/bin
+
+
+
+# >> Auto-completion
+
+completion_path="$COMPLETION_PATH/_poetry"
+[[ -f "$completion_path" ]] || {
+    info "Setting up poetry completions..."
+
+    poetry completions zsh > $completion_path
+    chmod 755 $completion_path
+
+    info "May need to restart shell"
+}
