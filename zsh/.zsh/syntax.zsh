a24c18ef672cddde8a94ca994be1f632863dd7ae
Oliver Marshall <olivershawmarshall@gmail.com>
2022-09-12 15:17:54 +0100

    Fix bug in zsh-syntax-hightlighting

--- zsh/.zsh/syntax.zsh
+++ zsh/.zsh/syntax.zsh
@@ -3,7 +3,7 @@
 
 # >> Load plugin
 
-. /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "$(brew-prefix zsh-syntax-highlighting)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 
 
