880b2a1d882d8b10e4f658866de54833a4dc7c04
Oliver Marshall <olivershawmarshall@gmail.com>
2022-09-21 23:57:05 +0100

    Add terraform package (maybe remove?)
    Maybe I should just have a better way of adding completions to the zsh
    package rather than lots of small opt-in packages?

---
+++ terraform/.zsh/terraform.zsh
@@ -0,0 +1,13 @@
+# >> Terraform
+
+
+# >> Completions
+
+# Only install the completions if terraform is installed
+command -v terraform 1>/dev/null && {
+    autoload -U +X bashcompinit
+    bashcompinit
+
+    complete -o nospace -C $(which terraform) terraform
+}
+
