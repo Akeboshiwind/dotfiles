fe7bd353b88db1835d2085f3be3b2e0366761bdb
Oliver Marshall <olivershawmarshall@gmail.com>
2022-10-24 10:17:03 +0100

    Add a binding to select shadowcljs session

--- nvim/.config/nvim/lua/config/conjure.lua
+++ nvim/.config/nvim/lua/config/conjure.lua
@@ -18,6 +18,76 @@ g["conjure#filetypes"] = { "clojure" }
 g["conjure#client#clojure#nrepl#mapping#session_select"] = false
 
 
+
+-- >> Custom commands
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local shadow_select = function(opts)
+    opts = opts or {}
+
+    -- A set used to de-duplicate the entries
+    -- To use a table as a set, use the keys as values
+    -- # Insertion:
+    -- set[value] = true
+    -- # Contains?
+    -- if set[value] then print("contains") end
+    -- See: https://www.lua.org/pil/11.5.html
+    local entry_cache = {}
+
+    -- Does three things to the `ps aux` output:
+    --  - Filters for shadow-cljs watch commands
+    --  - Returns the app name
+    --  - De-duplicates the results
+    opts.entry_maker = function (entry)
+        -- NOTE: Have to put the `-` in a set for some reason...
+        local app = entry:match("shadow[-]cljs watch (%w*)")
+
+        -- Skip entries that don't match
+        if not app then
+            return nil
+        end
+
+        -- Cache the entry
+        if entry_cache[app] then
+            return nil
+        end
+        entry_cache[app] = true
+
+        -- Return the app
+        return {
+            value = app,
+            display = app,
+            ordinal = app,
+        }
+    end
+
+    pickers.new(opts, {
+        prompt_title = "colours",
+        finder = finders.new_oneshot_job(
+            { "ps", "aux" },
+            opts
+        ),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function (prompt_bufnr, _)
+            -- When an app is selected, run ConjureShadowSelect
+            actions.select_default:replace(function ()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                local app = selection.value
+
+                vim.cmd(string.format("ConjureShadowSelect %s", app))
+            end)
+            return true
+        end
+    }):find()
+end
+
+
 -- >> Document Mappings
 
 local wk = require("which-key")
@@ -85,7 +155,7 @@ wk.register({
         l = "List",
         n = "Next",
         p = "Previous",
-        s = "Prompt to select",
+        S = { shadow_select, "Select" },
     },
     t = {
         name = "test",
