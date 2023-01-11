43b609fc069b0f056c0b9ab5a5b8d43c3c4654c9
Oliver Marshall <olivershawmarshall@gmail.com>
2023-01-11 20:48:10 +0000

    Load OPENAI_API_KEY from config file

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -9,7 +9,7 @@ local M = {
     },
 }
 
-function file_exists(path)
+function M.file_exists(path)
     local f = io.open(path, "rb")
     if f then
         f:close()
@@ -17,14 +17,28 @@ function file_exists(path)
     return f ~= nil
 end
 
-function M.config()
+-- Assumes there is a file ~/.config/openai/api_key
+-- That contains a single line with the api key on it
+function M.load_api_key()
     local api_key_path = vim.fn.expand("$HOME") .. "/.config/openai/api_key"
-    if not file_exists(api_key_path) then
+
+    if not M.file_exists(api_key_path) then
         vim.notify("ChatGPT: API key not found at " .. api_key_path, vim.log.levels.WARN)
+        return nil
+    end
+
+    local api_key = io.open(api_key_path):read("*l")
+
+    return api_key
+end
+
+function M.config()
+    local api_key = M.load_api_key()
+    if api_key == nil then
         return
     end
 
-    vim.env.OPENAI_API_KEY = io.open(api_key_path):read("*a")
+    vim.env.OPENAI_API_KEY = api_key
 
     local chatgpt = require("chatgpt")
     chatgpt.setup({})
