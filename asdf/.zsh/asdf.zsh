08e57c31dbe9a0025dc0fde5d47aab9d96729560
Oliver Marshall <olivershawmarshall@gmail.com>
2023-06-01 17:44:15 +0100

    fix(asdf): update asdf.sh location

--- asdf/.zsh/asdf.zsh
+++ asdf/.zsh/asdf.zsh
@@ -1,21 +1,6 @@
 # >> asdf
 
 
-# >> Config
-
-prefix=$(brew-prefix asdf)
-asdf_completion_path=$COMPLETION_PATH/_asdf
-
-
-
 # >> Setup
 
-. $prefix/asdf.sh
-
-
-
-# >> Completions
-
-[ -e "$asdf_completion_path" ] || {
-    cp $prefix/share/zsh/site-functions/_asdf $asdf_completion_path
-}
+. "$(brew-prefix asdf)/libexec/asdf.sh"
