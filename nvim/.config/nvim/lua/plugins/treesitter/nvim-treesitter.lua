625f65008dec66fab3fb429ddfead1af43c580cc
Oliver Marshall <olivershawmarshall@gmail.com>
2023-06-12 11:56:23 +0100

    fix(nvim): revert to working treesitter version

--- nvim/.config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
@@ -11,28 +11,30 @@ local M = {
         --     cmd = "TSPlaygroundToggle",
         -- },
     },
-    opts = {
-        ensure_installed = { "comment", "regex" },
-        auto_install = true,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "comment", "regex" },
+            auto_install = true,
 
-        highlight = {
-            enable = true,
-        },
+            highlight = {
+                enable = true,
+            },
 
-        indent = {
-            enable = false,
-        },
+            indent = {
+                enable = false,
+            },
 
-        -- playground = {
-        --     enable = true,
-        -- },
+            -- playground = {
+            --     enable = true,
+            -- },
 
-        query_linter = {
-            enable = true,
-            use_virtual_text = true,
-            lint_events = { "BufWrite", "CursorHold" },
-        },
-    },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+        })
+    end,
 }
 
 return M
