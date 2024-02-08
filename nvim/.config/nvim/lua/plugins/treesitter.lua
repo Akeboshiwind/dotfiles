778ffd2533f45d409c3a3d9d0826ef80c73b4ef5
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-08 22:34:27 +0000

    refactor(nvim): inline treesitter stuff in one file

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+-- plugins/treesitter.lua
+
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- dir = '~/prog/prog/assorted/nvim-treesitter',
+        -- branch = '0.5-compat',
+        build = ":TSUpdate",
+        dependencies = {
+            -- {
+            --     "nvim-treesitter/playground",
+            --     cmd = "TSPlaygroundToggle",
+            -- },
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "comment", "regex" },
+                auto_install = true,
+
+                highlight = {
+                    enable = true,
+                },
+
+                indent = {
+                    enable = false,
+                },
+
+                -- playground = {
+                --     enable = true,
+                -- },
+
+                query_linter = {
+                    enable = true,
+                    use_virtual_text = true,
+                    lint_events = { "BufWrite", "CursorHold" },
+                },
+            })
+        end,
+    },
+}
--- nvim/.config/nvim/lua/plugins/treesitter/init.lua
+++
@@ -1,5 +0,0 @@
--- plugins/treesitter/init.lua
-
-return {
-    { import = "plugins/treesitter/nvim-treesitter" },
-}
