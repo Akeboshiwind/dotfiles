956200ff68df3f7272820754b3ff4b725005e42b
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-08 22:39:54 +0000

    feat(nvim): remove unused neodev

--- nvim/.config/nvim/lua/plugins/lang/init.lua
+++ nvim/.config/nvim/lua/plugins/lang/init.lua
@@ -3,7 +3,6 @@
 return {
     "gurpreetatwal/vim-avro",
     { import = "plugins.lang.conjure" },
-    { import = "plugins.lang.neodev" },
     { import = "plugins.lang.python-tools" },
     { import = "plugins.lang.rust-tools" },
 }
