7d7efcd1c7d7f1fac2bc3ffa4142eae65d8d89b1
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-12 10:20:47 +0000

    feat(nvim): conjure switch to using default conjure binding descriptions

--- nvim/.config/nvim/lua/plugins/lang/conjure.lua
+++ nvim/.config/nvim/lua/plugins/lang/conjure.lua
@@ -20,6 +20,25 @@ return {
         build = "cargo build --release",
     },
     {
+        "folke/which-key.nvim",
+        opts = {
+            -- TODO: These don't work, why?
+            defaults = {
+                ["<leader>l"] = { name = "log" },
+                ["<leader>e"] = { name = "eval" },
+                ["<leader>c"] = { name = "display as comment" },
+                ["<leader>g"] = { name = "goto" },
+
+                -- Clojure nrepl specific
+                ["<leader>G"] = { name = "git" },
+                ["<leader>v"] = { name = "view" },
+                ["<leader>s"] = { name = "session" },
+                ["<leader>t"] = { name = "test" },
+                ["<leader>r"] = { name = "refresh" },
+            },
+        },
+    },
+    {
         "Olical/conjure",
         tag = "v4.50.0",
         dependencies = {
@@ -119,31 +138,7 @@ return {
 
             -- Base Conjure Mappings
             wk.register({
-                l = {
-                    name = "log",
-                    s = "Open in new horizontal split window",
-                    v = "Open in new vertical split window",
-                    t = "Open in new tab",
-                    q = "Close all visible windows in current tab",
-                    r = "Soft reset",
-                    R = "Hard reset",
-                },
-                E = "Evaluate given motion",
                 e = {
-                    name = "eval",
-                    e = "Form under the cursor",
-                    r = "Root form under the cursor",
-                    w = "Word under the cursor",
-                    c = {
-                        name = "display as comment",
-                        e = "Form under the cursor",
-                        r = "Root form under the cursor",
-                        w = "Word under the cursor",
-                    },
-                    ["!"] = "Replacing the Form under the cursor",
-                    m = "Form at the given mark",
-                    f = "File from disk",
-                    b = "Current buffer",
                     g = {
                         ":ConjureEval (user/go!)<CR>",
                         "user/go!",
@@ -160,55 +155,8 @@ return {
                         "clerk/show!",
                     },
                 },
-                g = {
-                    name = "goto",
-                    d = "Definition",
-                },
-            }, { prefix = "<leader>" })
-
-            wk.register({
-                E = "Evaluate selection",
-            }, { prefix = "<leader>", mode = "v" })
-
-            -- Clojure Nrepl Client Mappings
-            wk.register({
-                c = {
-                    name = "connection",
-                    d = "Disconnect current",
-                    f = "Connect",
-                },
-                ei = "Interrupt oldest",
-                v = {
-                    name = "view",
-                    e = "Last exception",
-                    ["1"] = "Most recent evaluation",
-                    ["2"] = "2nd most recent evaluation",
-                    ["3"] = "3rd most recent evaluation",
-                    s = "Source of symbol under cursor",
-                },
                 s = {
-                    name = "session",
-                    c = "Clone",
-                    f = "Create fresh",
-                    q = "Close current",
-                    Q = "Close all",
-                    l = "List",
-                    n = "Next",
-                    p = "Previous",
                     S = { shadow_select, "Conjure Select Shadowcljs Environment" },
-                },
-                t = {
-                    name = "test",
-                    a = "Run all loaded tests",
-                    n = "Run tests in namespace",
-                    N = "Run tests in testing namespace",
-                    c = "Run under cursor",
-                },
-                r = {
-                    name = "refresh",
-                    r = "Changed namespaces",
-                    a = "All, even unchanged",
-                    c = "Clear refresh cache",
                 },
             }, { prefix = "<leader>" })
         end,
