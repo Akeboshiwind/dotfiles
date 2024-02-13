fbbf7597fbdb5a96d477a74a65ca791e7890df26
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-13 12:17:11 +0000

    feat(nvim): disable auto formatting in clojure

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -7,7 +7,15 @@ return {
         cmd = { "ConformInfo" },
         opts = {
             formatters_by_ft = {},
-            format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            format_on_save = function(bufnr)
+                local opts = { timeout_ms = 500, lsp_fallback = true }
+
+                if vim.bo[bufnr].filetype == "clojure" then
+                    opts.lsp_fallback = false
+                end
+
+                return opts
+            end,
             formatters = {},
         },
     },
