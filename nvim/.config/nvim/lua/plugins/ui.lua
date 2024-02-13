3c4b39444ef1f137e5ed46efeef3921a7f731420
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-13 10:42:22 +0000

    refactor(nvim): combine things into ui.lua

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -3,6 +3,43 @@ local util = require("util")
 
 return {
     {
+        "rebelot/kanagawa.nvim",
+        enable = true,
+        priority = 1000, -- Load early
+        config = function()
+            require("kanagawa").setup({
+                -- dim inactive window `:h hl-NormalNC`
+                dimInactive = true,
+            })
+
+            vim.cmd([[colorscheme kanagawa]])
+        end,
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "kyazdani42/nvim-web-devicons",
+        },
+        opts = {
+            sections = {
+                lualine_a = { "filename" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "searchcount" },
+                lualine_x = {
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+                lualine_y = {},
+                lualine_z = { "location" },
+            },
+        },
+    },
+
+    {
         "rcarriga/nvim-notify",
         init = function()
             util.on_very_lazy(function()
@@ -23,5 +60,17 @@ return {
                 return math.floor(vim.o.columns * 0.75)
             end,
         },
-    }
+    },
+
+    {
+        "alexghergh/nvim-tmux-navigation",
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            {"<C-h>", function() require("nvim-tmux-navigation").NvimTmuxNavigateLeft() end,  desc="Navigate Left" },
+            {"<C-j>", function() require("nvim-tmux-navigation").NvimTmuxNavigateDown() end,  desc="Navigate Left" },
+            {"<C-k>", function() require("nvim-tmux-navigation").NvimTmuxNavigateUp() end,    desc="Navigate Left" },
+            {"<C-l>", function() require("nvim-tmux-navigation").NvimTmuxNavigateRight() end, desc="Navigate Left" },
+        },
+    },
 }
