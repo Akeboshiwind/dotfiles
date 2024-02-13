1e50dee78c27a847f635c6382f3059a9524f5e02
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-13 08:09:57 +0000

    refactor(nvim): fmt

--- nvim/.config/nvim/lua/util.lua
+++ nvim/.config/nvim/lua/util.lua
@@ -26,13 +26,13 @@ end
 M.lsp = {}
 
 function M.lsp.on_attach(on_attach)
-  vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local buffer = args.buf ---@type number
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_attach(client, buffer)
-    end,
-  })
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local buffer = args.buf ---@type number
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            on_attach(client, buffer)
+        end,
+    })
 end
 
 return M
