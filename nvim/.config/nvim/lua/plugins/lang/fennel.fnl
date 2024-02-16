80722aacca76b50b2f2d86b9025a84df4e0613bc
Oliver Marshall <olivershawmarshall@gmail.com>
2024-02-16 14:42:15 +0000

    feat(nvim): add fennel-language-server

--- nvim/.config/nvim/lua/plugins/lang/fennel.fnl
+++ nvim/.config/nvim/lua/plugins/lang/fennel.fnl
@@ -1,6 +1,25 @@
 ; plugins/lang/fennel.lua
+(local {: autoload} (require :nfnl.module))
+(local {: update} (autoload :nfnl.core))
+(local lspconfig (require :lspconfig))
 
 [{1 :Olical/nfnl
   :ft "fennel"}
  {1 :Olical/conjure
-  :ft ["fennel"]}]
+  :ft ["fennel"]}
+ {1 :williamboman/mason.nvim
+  :opts (fn [_ opts]
+          (-> opts
+              (update :ensure-installed #(or $ []))
+              (update :ensure-installed
+                #(vim.list_extend $ ["fennel-language-server"]))))}
+ {1 :neovim/nvim-lspconfig
+  :opts {:servers
+         {:fennel_language_server
+          {:filetypes [:fennel]
+           :root_dir (lspconfig.util.root_pattern "lua" "fnl")
+           :single_file_support true
+           :settings
+           {:fennel
+            {:diagnostics {:globals [:vim :jit :comment]}
+             :workspace {:library (vim.api.nvim_list_runtime_paths)}}}}}}}]
--- nvim/.config/nvim/lua/plugins/lang/fennel.lua
+++ nvim/.config/nvim/lua/plugins/lang/fennel.lua
@@ -1,2 +1,16 @@
 -- [nfnl] Compiled from lua/plugins/lang/fennel.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"Olical/nfnl", ft = "fennel"}, {"Olical/conjure", ft = {"fennel"}}}
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local _local_2_ = autoload("nfnl.core")
+local update = _local_2_["update"]
+local lspconfig = require("lspconfig")
+local function _3_(_, opts)
+  local function _4_(_241)
+    return (_241 or {})
+  end
+  local function _5_(_241)
+    return vim.list_extend(_241, {"fennel-language-server"})
+  end
+  return update(update(opts, "ensure-installed", _4_), "ensure-installed", _5_)
+end
+return {{"Olical/nfnl", ft = "fennel"}, {"Olical/conjure", ft = {"fennel"}}, {"williamboman/mason.nvim", opts = _3_}, {"neovim/nvim-lspconfig", opts = {servers = {fennel_language_server = {filetypes = {"fennel"}, root_dir = lspconfig.util.root_pattern("lua", "fnl"), single_file_support = true, settings = {fennel = {diagnostics = {globals = {"vim", "jit", "comment"}}, workspace = {library = vim.api.nvim_list_runtime_paths()}}}}}}}}
