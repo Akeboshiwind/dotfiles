95fb509f387d72cbce006d174c58d8b7b980da86
Oliver Marshall <olivershawmarshall@gmail.com>
2024-03-15 10:39:08 +0000

    feat(nvim): add flash.nvim

---
+++ nvim/.config/nvim/lua/plugins/flash.fnl
@@ -0,0 +1,29 @@
+; plugins/flash.fnl
+(local {: autoload} (require :nfnl.module))
+(local flash (autoload :flash))
+
+[{1 :folke/flash.nvim
+  :event :VeryLazy
+  :opts {:modes {:char {:keys [; So that `fd` still works as <esc>
+                               ; TODO: Is there another method?
+                               (comment :f) :F
+                               :t :T]}}}
+  :keys [{1 "s" 2 #(flash.jump)
+          :mode [:n :x :o]
+          :desc "Flash"}
+         {1 "S" 2 #(flash.treesitter)
+          :mode [:n :x :o]
+          :desc "Flash Treesitter"}
+         {1 "*" 2 #(flash.jump {:pattern (vim.fn.expand "<cword>")})
+          :mode [:n :x :o]
+          :desc "Flash under cursor"}
+         {1 "r" 2 #(flash.remote)
+          :mode "o"
+          :desc "Remote Flash"}
+         {1 "R" 2 #(flash.treesitter_search)
+          :mode [:o :x]
+          :desc "Treesitter Search"}
+         {1 "<c-s>" 2 #(flash.toggle)
+          :mode [:c]
+          :desc "Toggle Flash Search"}]}]
+
---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,24 @@
+-- [nfnl] Compiled from lua/plugins/flash.fnl by https://github.com/Olical/nfnl, do not edit.
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local flash = autoload("flash")
+--[[ "f" ]]
+local function _2_()
+  return flash.jump()
+end
+local function _3_()
+  return flash.treesitter()
+end
+local function _4_()
+  return flash.jump({pattern = vim.fn.expand("<cword>")})
+end
+local function _5_()
+  return flash.remote()
+end
+local function _6_()
+  return flash.treesitter_search()
+end
+local function _7_()
+  return flash.toggle()
+end
+return {{"folke/flash.nvim", event = "VeryLazy", opts = {modes = {char = {keys = {nil, "F", "t", "T"}}}}, keys = {{"s", _2_, mode = {"n", "x", "o"}, desc = "Flash"}, {"S", _3_, mode = {"n", "x", "o"}, desc = "Flash Treesitter"}, {"*", _4_, mode = {"n", "x", "o"}, desc = "Flash under cursor"}, {"r", _5_, mode = "o", desc = "Remote Flash"}, {"R", _6_, mode = {"o", "x"}, desc = "Treesitter Search"}, {"<c-s>", _7_, mode = {"c"}, desc = "Toggle Flash Search"}}}}
