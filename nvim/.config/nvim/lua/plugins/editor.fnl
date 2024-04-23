1d71f3c4ba31e3bd530297f026aa015f322e0988
Oliver Marshall <olivershawmarshall@gmail.com>
2024-04-23 10:37:50 +0100

    feat(nvim): add vim-matchup & guess-indent.nvim

---
+++ nvim/.config/nvim/lua/plugins/editor.fnl
@@ -0,0 +1,9 @@
+; plugins/editor.fnl
+
+[{1 :nvim-treesitter/nvim-treesitter
+  :opts {:matchup {:enable true
+                   :include_match_words true}}}
+ :andymass/vim-matchup
+
+ {1 :nmac427/guess-indent.nvim
+  :opts {}}]
---
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from lua/plugins/editor.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"nvim-treesitter/nvim-treesitter", opts = {matchup = {enable = true, include_match_words = true}}}, "andymass/vim-matchup", {"nmac427/guess-indent.nvim", opts = {}}}
