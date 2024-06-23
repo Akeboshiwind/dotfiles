54ffc9537e3d88ec6ebd6e49eede1c231b8e76d2
Oliver Marshall <olivershawmarshall@gmail.com>
2024-06-23 18:22:58 +0100

    refactor(nvim): use :opt for copilot

--- nvim/.config/nvim/lua/plugins/copilot.fnl
+++ nvim/.config/nvim/lua/plugins/copilot.fnl
@@ -1,9 +1,7 @@
 ; plugins/copilot.fnl
-(local {: autoload} (require :nfnl.module))
-(local copilot-cmp (autoload :copilot_cmp))
 
 [{1 :zbirenbaum/copilot-cmp
-  :config #(copilot-cmp.setup)}
+  :opts {}}
  {1 :zbirenbaum/copilot.lua
   :event :VeryLazy
   :dependencies [:zbirenbaum/copilot-cmp]
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,2 @@
 -- [nfnl] Compiled from lua/plugins/copilot.fnl by https://github.com/Olical/nfnl, do not edit.
-local _local_1_ = require("nfnl.module")
-local autoload = _local_1_["autoload"]
-local copilot_cmp = autoload("copilot_cmp")
-local function _2_()
-  return copilot_cmp.setup()
-end
-return {{"zbirenbaum/copilot-cmp", config = _2_}, {"zbirenbaum/copilot.lua", event = "VeryLazy", dependencies = {"zbirenbaum/copilot-cmp"}, opts = {panel = {enabled = false}, suggestion = {enabled = false}}}}
+return {{"zbirenbaum/copilot-cmp", opts = {}}, {"zbirenbaum/copilot.lua", event = "VeryLazy", dependencies = {"zbirenbaum/copilot-cmp"}, opts = {panel = {enabled = false}, suggestion = {enabled = false}}}}
