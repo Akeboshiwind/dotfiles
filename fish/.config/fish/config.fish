b0340105cdcf8a0cc3237373f658daf9eb3a66a7
Oliver Marshall <olivershawmarshall@gmail.com>
2024-08-18 11:02:56 +0100

    feat(fish): set as startup shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Start tmux if not already running
+    if command -qv tmux; and test -z "$TMUX"
+        tmux attach || tmux new
+    end
 end
+
+# Disable greeting
+set fish_greeting ""
+
+# TODO: Prompt to `fish_update_completions` occasionally
