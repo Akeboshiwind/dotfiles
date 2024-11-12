8f554a761d11b3e8d085b398a28b7c1bafab912a
Oliver Marshall <olivershawmarshall@gmail.com>
2024-11-12 12:51:22 +0000

    feat(nvim): disable copilot by default

--- nvim/.config/nvim/lua/plugins/copilot.fnl
+++ nvim/.config/nvim/lua/plugins/copilot.fnl
@@ -1,9 +1,18 @@
 ; plugins/copilot.fnl
+(local {: autoload} (require :nfnl.module))
+(local copilot (autoload :copilot))
+(local copilot-cmd (autoload :copilot.command))
 
 [{1 :zbirenbaum/copilot-cmp
   :opts {}}
  {1 :zbirenbaum/copilot.lua
   :event :VeryLazy
   :dependencies [:zbirenbaum/copilot-cmp]
-  :opts {:panel {:enabled false}
-         :suggestion {:enabled false}}}]
+  :config (fn []
+            (copilot.setup
+              {:panel {:enabled false}
+               :suggestion {:enabled false}})
+            ; Disable by default
+            ; Enable using `:Copilot enable`
+            ; TODO: Fix that this prints `[Copilot] Offline` on startup
+            (copilot-cmd.disable))}]
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,2 +1,10 @@
 -- [nfnl] Compiled from lua/plugins/copilot.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"zbirenbaum/copilot-cmp", opts = {}}, {"zbirenbaum/copilot.lua", event = "VeryLazy", dependencies = {"zbirenbaum/copilot-cmp"}, opts = {panel = {enabled = false}, suggestion = {enabled = false}}}}
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local copilot = autoload("copilot")
+local copilot_cmd = autoload("copilot.command")
+local function _2_()
+  copilot.setup({panel = {enabled = false}, suggestion = {enabled = false}})
+  return copilot_cmd.disable()
+end
+return {{"zbirenbaum/copilot-cmp", opts = {}}, {"zbirenbaum/copilot.lua", event = "VeryLazy", dependencies = {"zbirenbaum/copilot-cmp"}, config = _2_}}
