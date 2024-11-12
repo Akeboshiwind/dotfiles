d4a33225859b0f2d6aaa62fa1188c91a3099db89
Oliver Marshall <olivershawmarshall@gmail.com>
2024-11-12 12:50:17 +0000

    docs(asdf): add some notes on asdf breaking in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,11 +5,26 @@ if status is-interactive
     end
 end
 
+
 # TODO: Prompt to `fish_update_completions` occasionally
 
 # >> Load some configs after everything else
 
-# This is mostly to work around asdf needing to be set *after* $fish_user_paths
+# I think this works but I'm not 100% sure
+# Basically, asdf shims weren't being picked up (`java -version` was erroring)
+# I *think* what was happening was that the shims were being added too late
+# Possibly $fish_user_paths is messing up the path order?
+# If so I'm not sure what I can do for that other than banning fish_set_path
+# Maybe wrap it so that asdf shims are always used?
 for config in $__fish_config_dir/conf.d.after/*
     source $config
 end
+
+
+# >> Note to future me: asdf shims broken
+# tags: asdf, shim, java
+
+# If you accidentially exit tmux completely then restart it, for some reason the asdf shims to to the back of the PATH
+# This means they get overshadowed by /usr/bin.
+# The solution is: Just restart Alacritty.
+# Not really sure why 🤷
