b15e2f5133679b338aa8e8aa44fb44743f88a65f
Oliver Marshall <olivershawmarshall@gmail.com>
2025-02-27 17:04:44 +0000

    feat(nvim): allow disabling format_on_save per ft

--- nvim/.config/nvim/lua/plugins/format.fnl
+++ nvim/.config/nvim/lua/plugins/format.fnl
@@ -1,5 +1,6 @@
 ; plugins/format.fnl
 (local {: autoload} (require :nfnl.module))
+(local {: get : assoc} (autoload :nfnl.core))
 (local conform (autoload :conform))
 
 [{1 :stevearc/conform.nvim
@@ -8,5 +9,14 @@
   :keys [{1 "<leader>F" 2 #(conform.format) :desc "Format buffer"}]
   :opts {:formatters_by_ft {}
          :formatters {}
-         :format_on_save {:timeout_ms 500
-                          :lsp_format "fallback"}}}]
+         :no_format_on_save {}
+         :format_on_save {:lsp_fallback true
+                          :timeout_ms 500}}
+  :config (fn [_ opts]
+            (let [format_on_save (get opts :format_on_save {})]
+              (-> opts
+                  (assoc :format_on_save #(let [ft (vim.api.nvim_buf_get_option $ "filetype")]
+                                            (when (not (get opts.no_format_on_save ft))
+                                              (_G.P "format_on_save")
+                                              format_on_save)))
+                  (conform.setup))))}]
--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,8 +1,24 @@
 -- [nfnl] Compiled from lua/plugins/format.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
+local _local_2_ = autoload("nfnl.core")
+local get = _local_2_["get"]
+local assoc = _local_2_["assoc"]
 local conform = autoload("conform")
-local function _2_()
+local function _3_()
   return conform.format()
 end
-return {{"stevearc/conform.nvim", event = {"BufWritePre"}, cmd = {"ConformInfo"}, keys = {{"<leader>F", _2_, desc = "Format buffer"}}, opts = {formatters_by_ft = {}, formatters = {}, format_on_save = {timeout_ms = 500, lsp_format = "fallback"}}}}
+local function _4_(_, opts)
+  local format_on_save = get(opts, "format_on_save", {})
+  local function _5_(_241)
+    local ft = vim.api.nvim_buf_get_option(_241, "filetype")
+    if not get(opts.no_format_on_save, ft) then
+      _G.P("format_on_save")
+      return format_on_save
+    else
+      return nil
+    end
+  end
+  return conform.setup(assoc(opts, "format_on_save", _5_))
+end
+return {{"stevearc/conform.nvim", event = {"BufWritePre"}, cmd = {"ConformInfo"}, keys = {{"<leader>F", _3_, desc = "Format buffer"}}, opts = {formatters_by_ft = {}, formatters = {}, no_format_on_save = {}, format_on_save = {lsp_fallback = true, timeout_ms = 500}}, config = _4_}}
