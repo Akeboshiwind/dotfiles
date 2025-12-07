e31a5a32ee85ca0771c217028e411c851667aa97
Oliver Marshall <olivershawmarshall@gmail.com>
2025-12-07 21:37:17 +0000

    fix(nvim): ignore vim global diagnostic

--- cfg/nvim/.config/nvim/lua/plugins/fennel.fnl
+++ cfg/nvim/.config/nvim/lua/plugins/fennel.fnl
@@ -1,3 +1,5 @@
 [{1 :Olical/nfnl :ft :fennel}
  {1 :nvim-treesitter/nvim-treesitter
-  :opts {:ensure_installed [:fennel]}}]
+  :opts {:ensure_installed [:fennel]}}
+ {1 :neovim/nvim-lspconfig
+  :opts {:servers {:fennel_language_server {:settings {:fennel {:diagnostics {:globals [:vim]}}}}}}}]
--- cfg/nvim/.config/nvim/lua/plugins/fennel.lua
+++ cfg/nvim/.config/nvim/lua/plugins/fennel.lua
@@ -1,2 +1,2 @@
 -- [nfnl] lua/plugins/fennel.fnl
-return {{"Olical/nfnl", ft = "fennel"}, {"nvim-treesitter/nvim-treesitter", opts = {ensure_installed = {"fennel"}}}}
+return {{"Olical/nfnl", ft = "fennel"}, {"nvim-treesitter/nvim-treesitter", opts = {ensure_installed = {"fennel"}}}, {"neovim/nvim-lspconfig", opts = {servers = {fennel_language_server = {settings = {fennel = {diagnostics = {globals = {"vim"}}}}}}}}}
