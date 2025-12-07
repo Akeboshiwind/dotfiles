9e151c45d03c8c899b7ac305a965e34679d2288d
Oliver Marshall <olivershawmarshall@gmail.com>
2025-12-07 22:16:40 +0000

    feat(nvim): switch to paredit

---
+++ cfg/nvim/.config/nvim/lua/plugins/clojure.fnl
@@ -0,0 +1,3 @@
+[{1 :nvim-paredit/nvim-paredit :enabled false}
+ {1 :eraserhd/parinfer-rust
+  :build "cargo build --release"}]
---
+++ cfg/nvim/.config/nvim/lua/plugins/clojure.lua
@@ -0,0 +1,2 @@
+-- [nfnl] lua/plugins/clojure.fnl
+return {{"nvim-paredit/nvim-paredit", enabled = false}, {"eraserhd/parinfer-rust", build = "cargo build --release"}}
