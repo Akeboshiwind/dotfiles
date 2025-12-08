6fb3229248847affeef1a89900757d4d95be553f
Oliver Marshall <olivershawmarshall@gmail.com>
2025-12-08 15:24:13 +0000

    fix(nvim): remove project.clj as root_marker

--- cfg/nvim/.config/nvim/lua/plugins/clojure.fnl
+++ cfg/nvim/.config/nvim/lua/plugins/clojure.fnl
@@ -1,3 +1,5 @@
 [{1 :nvim-paredit/nvim-paredit :enabled false}
  {1 :eraserhd/parinfer-rust
-  :build "cargo build --release"}]
+  :build "cargo build --release"}
+ {1 :neovim/nvim-lspconfig
+  :opts {:servers {:clojure_lsp {:root_markers [:deps.edn :build.boot :shadow-cljs.edn :.git :bb.edn]}}}}]
--- cfg/nvim/.config/nvim/lua/plugins/clojure.lua
+++ cfg/nvim/.config/nvim/lua/plugins/clojure.lua
@@ -1,2 +1,2 @@
 -- [nfnl] lua/plugins/clojure.fnl
-return {{"nvim-paredit/nvim-paredit", enabled = false}, {"eraserhd/parinfer-rust", build = "cargo build --release"}}
+return {{"nvim-paredit/nvim-paredit", enabled = false}, {"eraserhd/parinfer-rust", build = "cargo build --release"}, {"neovim/nvim-lspconfig", opts = {servers = {clojure_lsp = {root_markers = {"deps.edn", "build.boot", "shadow-cljs.edn", ".git", "bb.edn"}}}}}}
