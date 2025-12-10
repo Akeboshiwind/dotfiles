c38903e01654526ab29372c36a3e4ca22068dc78
Oliver Marshall <olivershawmarshall@gmail.com>
2025-12-10 13:05:31 +0000

    feat(nvim): creat :Nohl shortcut for :nohl

---
+++ cfg/nvim/.config/nvim/lua/config/autocmds.fnl
@@ -0,0 +1,1 @@
+(vim.api.nvim_create_user_command :Nohl (fn [] (vim.cmd :nohl)) {})
--- cfg/nvim/.config/nvim/lua/config/autocmds.lua
+++ cfg/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,2 +1,5 @@
 -- [nfnl] lua/config/autocmds.fnl
-
+local function _1_()
+  return vim.cmd("nohl")
+end
+return vim.api.nvim_create_user_command("Nohl", _1_, {})
