b76f2460639b34b09f6d6bb94b959c927d3f023f
Oliver Marshall <olivershawmarshall@gmail.com>
2026-01-08 15:29:03 +0000

    feat(nvim): try out JetBrains kotlin-lsp

--- cfg/nvim/.config/nvim/lua/plugins/kotlin.fnl
+++ cfg/nvim/.config/nvim/lua/plugins/kotlin.fnl
@@ -1,4 +1,9 @@
 [; Requires building and installing kotlin adapter from source
  {1 :nvim-neotest/neotest
   :dependencies [{1 :ake-forks/neotest-kotlin :branch "v2.0.0-backticks-fix"}]
-  :opts {:adapters {:neotest-kotlin {}}}}]
+  :opts {:adapters {:neotest-kotlin {}}}}
+
+ ; Use JetBrains' kotlin-lsp instead of fwcd/kotlin-language-server
+ {1 :neovim/nvim-lspconfig
+  :opts {:servers {:kotlin_language_server {:enabled false}
+                   :kotlin_lsp {}}}}]
--- cfg/nvim/.config/nvim/lua/plugins/kotlin.lua
+++ cfg/nvim/.config/nvim/lua/plugins/kotlin.lua
@@ -1,2 +1,2 @@
 -- [nfnl] lua/plugins/kotlin.fnl
-return {{"nvim-neotest/neotest", dependencies = {{"ake-forks/neotest-kotlin", branch = "v2.0.0-backticks-fix"}}, opts = {adapters = {["neotest-kotlin"] = {}}}}}
+return {{"nvim-neotest/neotest", dependencies = {{"ake-forks/neotest-kotlin", branch = "v2.0.0-backticks-fix"}}, opts = {adapters = {["neotest-kotlin"] = {}}}}, {"neovim/nvim-lspconfig", opts = {servers = {kotlin_language_server = {enabled = false}, kotlin_lsp = {}}}}}
