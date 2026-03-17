1f46a51064947c64521ae03dc085fa09b122d748
Oliver Marshall <olivershawmarshall@gmail.com>
2026-03-17 08:49:13 +0000

    feat: allow :clj in :assert

--- src/actions/assert.clj
+++ src/actions/assert.clj
@@ -1,38 +1,44 @@
 (ns actions.assert
   (:require [actions :as a]
             [babashka.process :as process]
+            [clojure.string :as str]
             [display :as d]
             [outcome :as o]))
 
 (defmethod a/requires :assert [_] nil)
 
+(defn- eval-clj
+  "Evaluate a Clojure form with assert utility functions available."
+  [form]
+  (binding [*ns* (find-ns 'actions.assert)]
+    (eval form)))
+
+(defn- run-check
+  "Run an assertion check. Returns true if passed, false if failed."
+  [{:keys [path src clj]}]
+  (cond
+    clj  (boolean (eval-clj clj))
+    path (zero? (:exit (apply process/shell {:out :string :err :string :continue true} ["bash" path])))
+    src  (zero? (:exit (process/shell {:out :string :err :string :continue true} "bash" "-c" src)))))
+
 (defmethod a/check :assert [_ key opts]
-  (let [{:keys [path src]} opts]
-    (if-not (or path src)
-      (o/error "Either :path or :src required")
-      (let [cmd (if path ["bash" path] ["bash" "-c" src])
-            result (apply process/shell {:out :string :err :string :continue true} cmd)]
-        (if (zero? (:exit result))
-          o/satisfied
-          (cond-> (o/error (or (:message opts) "assertion failed"))
-            (:instructions opts) (assoc :detail (:instructions opts))))))))
+  (if-not (or (:path opts) (:src opts) (:clj opts))
+    (o/error "Either :path, :src, or :clj required")
+    (if (run-check opts)
+      o/satisfied
+      (cond-> (o/error (or (:message opts) "assertion failed"))
+        (:instructions opts) (assoc :detail (:instructions opts))))))
 
 (defmethod a/install! :assert [_ opts items]
   (d/section "Checking assertions"
-    (map (fn [[k {:keys [path src message instructions]}]]
-           (let [cmd (cond
-                       path ["bash" path]
-                       src  ["bash" "-c" src]
-                       :else (throw (ex-info "Assert must have :path or :src"
-                                             {:key k})))
-                 {:keys [exit]} (a/exec! opts cmd)]
-             (if (zero? exit)
-               {:action [:assert k]
-                :label (name k)
-                :status :ok}
-               {:action [:assert k]
-                :label (name k)
-                :status :error
-                :message (or message "assertion failed")
-                :detail instructions})))
+    (map (fn [[k {:keys [message instructions] :as cfg}]]
+           (if (run-check cfg)
+             {:action [:assert k]
+              :label (name k)
+              :status :ok}
+             {:action [:assert k]
+              :label (name k)
+              :status :error
+              :message (or message "assertion failed")
+              :detail instructions}))
          items)))
